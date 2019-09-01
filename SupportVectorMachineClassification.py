a5c929573ba9fc983df8928c947918b2c81c6d0c
Sumon <sdtechie22@gmail.com>
2019-09-01 09:23:12 +0530

    Added Classification

--- SupportVectorMachineClassification.py
+++ SupportVectorMachineClassification.py
@@ -1,8 +1,8 @@
-#KNN (K-Nearest Neighbors) Classification
+# Support Vector Machine (SVM)
 
-# Import the required libraries and the collected dataset
-import numpy as np
+# Import the required libraries and collected dataset
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plotter
 collectedDataset = pd.read_csv('Car_Purchase.csv')
 X = collectedDataset.iloc[:, [2, 3]].values
@@ -18,31 +18,31 @@ standardScaler = StandardScaler()
 X_train = standardScaler.fit_transform(X_train)
 X_test = standardScaler.transform(X_test)
 
-# Fit the KNN classifier to the Training set
-from sklearn.neighbors import KNeighborsClassifier
-classifier=KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
-classifier.fit(X_train,Y_train)
+# Fit the SVM to the Training set
+from sklearn.svm import SVC
+suuportVectorClassifier=SVC(kernel='linear',random_state=0)
+suuportVectorClassifier.fit(X_train,Y_train)
 
 # Predict the Test set results
-Y_pred = classifier.predict(X_test)
+Y_predict = suuportVectorClassifier.predict(X_test)
 
 # Make the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(Y_test, Y_pred)
+confusionMatrix = confusion_matrix(Y_test, Y_predict)
 
 # Visualize the Training set results
 from matplotlib.colors import ListedColormap
 X_set, Y_set = X_train, Y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plotter.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+plotter.contourf(X1, X2, suuportVectorClassifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plotter.xlim(X1.min(), X1.max())
 plotter.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(Y_set)):
     plotter.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plotter.title('K-NN (Training set)')
+plotter.title('Support Vector Machine (Training set)')
 plotter.xlabel('Age')
 plotter.ylabel('Estimated Salary')
 plotter.legend()
@@ -53,19 +53,17 @@ from matplotlib.colors import ListedColormap
 X_set, Y_set = X_test, Y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plotter.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+plotter.contourf(X1, X2, suuportVectorClassifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plotter.xlim(X1.min(), X1.max())
 plotter.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(Y_set)):
     plotter.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plotter.title('K-NN (Test set)')
+plotter.title('Support Vector Machine (Test set)')
 plotter.xlabel('Age')
 plotter.ylabel('Estimated Salary')
 plotter.legend()
 plotter.show()
-
-
 
 
